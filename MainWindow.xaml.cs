96b5b0858ab661877616616a48cbd36563e0871b
brenton ferullo <brenton.ferullo@me.com>
2020-07-29 12:16:59 -0500

    added image processing

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,81 @@ namespace ScorpCamp
     /// </summary>
     public partial class MainWindow : Window
     {
+        public const int PLAYERHEIGHT = 400;
+        //<Image x:Name="goni" HorizontalAlignment="Left" Height="463" VerticalAlignment="Top" Width="228" Source="standard.png" Margin="991,85,0,0" RenderTransformOrigin="3.292,0.792" Grid.Column="1"/>
+        public class Combatant
+        {
+            public Image pic;
+            public string Name;
+        }
+
+        Combatant player = new Combatant();
+        Combatant enemy = new Combatant();
+
         public MainWindow()
         {
             InitializeComponent();
+            player = setCriss();
+            player.pic = flipImage(player.pic);
+            enemy = setRatMaster();
+
+            var img = player.pic;
+            Grid.SetColumn(img, 0);
+            Grid.SetRow(img, 0);
+            GameArea.Children.Add(img);
+
+            img = enemy.pic;
+            Grid.SetColumn(img, 1);
+            Grid.SetRow(img, 0);
+            GameArea.Children.Add(img);
+        }
+
+        public Image flipImage(Image arg)
+        {
+            arg.FlowDirection = FlowDirection.RightToLeft;
+            return arg;
+        }
+
+        public Combatant setCriss()
+        {
+            Combatant rtrn = new Combatant();
+
+            Image img = new Image();
+            img.Name = "criss";
+            img.HorizontalAlignment = HorizontalAlignment.Left;
+            img.VerticalAlignment = VerticalAlignment.Top;
+            img.Height = PLAYERHEIGHT;
+            //goni.Width = 350;
+            Uri src = new Uri("crisswithagun.png", UriKind.Relative);
+            img.Source = new BitmapImage(src);
+            img.Margin = new Thickness(0, 40, 50, 0);
+
+            rtrn.Name = "Criss";
+            rtrn.pic = img;
+
+            return rtrn;
+        }
+
+        public Combatant setRatMaster()
+        {
+            Combatant rtrn = new Combatant();
+
+            Image img = new Image();
+            img.Name = "goni";
+            img.HorizontalAlignment = HorizontalAlignment.Right;
+            img.VerticalAlignment = VerticalAlignment.Top;
+            img.Height = PLAYERHEIGHT;
+            //goni.Width = 350;
+            Uri src = new Uri("standard.png", UriKind.Relative);
+            img.Source = new BitmapImage(src);
+            img.Margin = new Thickness(0, 40, 50, 0);
+
+            //goni.RenderTransformOrigin = new Point(3.292, 0.792);
+
+            rtrn.pic = img;
+            rtrn.Name = "The Rat Master";
+
+            return rtrn;
         }
     }
 }
